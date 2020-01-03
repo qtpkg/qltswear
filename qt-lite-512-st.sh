1b5b695642c9b1c6a36980832cf4d6817585f8a6
drswinghead <drswinghead@163.com>
2020-01-03 23:11:37 +0800

    inited

--- qt-lite-512-st.sh
+++ qt-lite-512-st.sh
@@ -1,13 +1,92 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 
-wget https://download.qt.io/official_releases/qt/5.12/5.12.6/submodules/qtbase-everywhere-src-5.12.6.tar.xz
+#sudo apt install -y libxcb1-dev libxcb-xkb-dev libx11-xcb-dev libxcb-cursor-dev libxcb-render0-dev
+# https://qtlite.com/
 
-tar xf qtbase-everywhere-src-5.12.6.tar.xz
+QTPFXDIR=/opt/qtlt512st
+QTSRCDIR=qtbase-everywhere-src-5.12.6
 
-mkdir build
-ls
+mkbuild=1
 
-cd build && ../qtbase-everywhere-src-5.12.6/configure -opensource -confirm-license -prefix /opt/qtlt512st -static -release -v -reduce-relocations -optimize-size -make libs -nomake tools -nomake examples -no-dbus -no-opengl -qpa xcb -qt-zlib -qt-libjpeg -qt-libpng -xcb -qt-xcb -qt-freetype -qt-pcre -qt-harfbuzz -no-sql-ibase -no-sql-psql -no-sql-mysql -no-sql-odbc -no-sql-tds
+if [ "$mkbuild" == "1" ]; then
+
+    wget https://download.qt.io/official_releases/qt/5.12/5.12.6/submodules/$QTSRCDIR.tar.xz
+
+    tar xf $QTSRCDIR.tar.xz
+
+    # integeration fcitx input method for static build
+    git clone https://github.com/telegramdesktop/fcitx $QTSRCDIR/src/plugins/platforminputcontexts/fcitx
+    echo "SUBDIRS += fcitx" >> $QTSRCDIR/src/plugins/platforminputcontexts/platforminputcontexts.pro
+
+    mkdir build
+    ls
+
+    cd build
+
+    ../$QTSRCDIR/configure \
+        -opensource -confirm-license -prefix /opt/qt512lt -static -release -v \
+        -reduce-relocations -optimize-size -make libs \
+        -nomake tools -nomake examples -nomake tests \
+        -dbus -no-opengl -qpa xcb -qt-zlib -qt-libjpeg -qt-libpng \
+        -xcb -qt-xcb -qt-freetype -qt-pcre -qt-harfbuzz \
+        -no-feature-fontconfig -no-feature-im -no-feature-libinput -no-feature-libinput-axis-api \
+        -no-sql-ibase -no-sql-psql -no-sql-mysql -no-sql-odbc -no-sql-tds \
+        -feature-abstractbutton -no-feature-accessibility -no-feature-accessibility-atspi-bridge \
+        -feature-action -feature-xrender -no-feature-cxx11_future \
+        -feature-c++11 -no-feature-c++14 -no-feature-c++1z -feature-c11 -feature-c99 \
+        -no-feature-glib -no-feature-gtk3 -no-feature-icu -no-feature-linuxfb -no-feature-directfb \
+        -no-feature-tslib -no-feature-vnc -no-feature-system-doubleconversion \
+        -no-feature-cups -no-feature-syslog -no-feature-syntaxhighlighter \
+        -no-feature-egl -no-feature-egl_x11 -no-feature-eglfs -no-feature-eglfs_brcm \
+        -no-feature-eglfs_egldevice -no-feature-eglfs_gbm -no-feature-eglfs_mali \
+        -no-feature-eglfs_openwfd -no-feature-eglfs_rcar -no-feature-eglfs_viv \
+        -no-feature-eglfs_viv_wl -no-feature-eglfs_vsp2 -no-feature-eglfs_x11 \
+        -no-feature-xcb-egl-plugin -no-feature-xcb-glx -no-feature-xcb-glx-plugin \
+        -no-feature-listwidget -no-feature-tablewidget -no-feature-treewidget \
+        -no-feature-mdiarea -no-feature-whatsthis  -no-feature-wizard -no-feature-dial \
+        -no-feature-textbrowser -no-feature-texthtmlparser -no-feature-textodfwriter \
+        -no-feature-texture_format_astc_experimental -no-feature-big_codecs \
+        -no-feature-colordialog -no-feature-fontdialog  -no-feature-calendarwidget \
+        -no-feature-printdialog  -no-feature-printpreviewdialog -no-feature-printpreviewwidget \
+        -no-feature-tuiotouch -no-feature-datawidgetmapper -no-feature-progressdialog \
+        -no-feature-vulkan -no-feature-xcb-xlib -no-feature-xcb-sm -no-feature-xlib \
+        -no-feature-splashscreen -no-feature-lcdnumber -no-feature-itemmodeltester \
+        -no-feature-concurrent -no-feature-xml -no-feature-testlib -no-feature-libudev \
+        -no-feature-sessionmanager -no-feature-evdev -no-feature-bearermanagement \
+        -no-feature-style-fusion -no-feature-style-windowsvista -no-feature-animation \
+        -no-feature-gestures -no-feature-effects -no-feature-graphicseffect -no-feature-graphicsview \
+        -no-feature-openssl -no-feature-openssl-linked -no-feature-ftp -no-feature-http \
+        -no-feature-networkdiskcache -no-feature-networkinterface -no-feature-networkproxy \
+        -no-feature-commandlineparser -no-feature-integrityfb -no-feature-integrityhid \
+        -no-feature-multiprocess -no-feature-process -no-feature-processenvironment \
+        -no-feature-statemachine
+
+
+    sudo make install_mkspecs
+    make -C src/ sub-3rdparty-pcre2-install_subtargets
+    make -C src/ sub-3rdparty-freetype-install_subtargets
+    make -C src/ sub-3rdparty-harfbuzzng-install_subtargets
+    make -C src/platformsupport/services/ install
+    make -C src/platformsupport/themes/ install
+    make -C src/platformsupport/edid/ install
+    make -C src/platformsupport/fontdatabases/ install
+    for mod in 'corelib gui widgets plugins'; do
+        sudo make -C src/$mod install
+    done
+    make install
+
+    cd ..
+fi
+
+mkdir $QTPFXDIR
+ls $QTPFXDIR -lh
+
+cp -a ./qt-lite-512-st.sh $QTPFXDIR/
+env > $QTPFXDIR/env.txt
+strip -sv $QTPFXDIR/lib/*.a
+tar jcfp qtlt512st.tar.xz $QTPFXDIR/
+ls -lh
+
 
---
+++ readme.md
@@ -0,0 +1,39 @@
+
+qt lite 编译最精简的 gui 库
+
+占内存要小
+占磁盘空间要小
+只包含 core gui widgets 三个包
+不包含GL相关插件与链接依赖
+不包含glib/gtk相关插件与链接依赖
+支持中文输入法
+
+基于 qtbase-everywhere包构建
+
+### 结果
+
+linux 64
+hello world button:
+12M 程序文件，未strip
+启动程序占用12M内存，放大窗口后占用在25M左右。
+
+libQt5Core.a: 12M
+libQt5Gui.a: 9.8M
+libQt5Widgets.a: 10M
+
+去掉系统库和C/C++标准库，只有6个库链接依赖
+libxcb.so libxkbcommon.so libXau.so libXdump.so libxcb-xkb.so libfreetype.so
+
+### 注意
+没有编译libfontconfig，不能自动搜索字体。而开启libfontconfig则，启动搜索字体变慢3-5秒，内存涨10M
+字体问题，需要链接一个.ttc/.ttf到 prefix/lib/fonts/目录下
+
+(已测试) https://github.com/telegramdesktop/fcitx
+放在qt 5.12源码树的 plugins/platforminputcontext/目录，在platforminputcontext.pro中添加与ibus一样的SUBDIR项，重新编译即可。
+
+(x这个是动态链接版本) 输入法问题，使用该静态qt库编译fcitx-qt5的libfcitxplatforminputcontext为静态库，然后链接到最终程序中
+编译https://github.com/telegramdesktop/fcitx 需要qt dbus模块，所以上面编译的库包含这个模块。
+
+(未测试)输入法整合到qt，https://git.mel.vin/telegram_desktop/upstream/commit/efa62ece723d36f6b28ecb0f1bf1ba84b57900f0?view=parallel
+
+
